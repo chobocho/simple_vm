0dc9187fe5e47532bb1d828027d9af4651d01f67
chobocho <chobocho99@gmail.com>
2023-03-17 23:28:56 +0900

    [Patch] Add CYCLE command [Patch] Add SQRT command [Patch] Update comments [Patch] Add more sample code

--- simple_vm/main.py
+++ simple_vm/main.py
@@ -3,7 +3,7 @@ from stack_machine import StackMachine
 
 def main():
     # Multiplying 1 through 10
-    program = [
+    program_10_factorial = [
         'PUSH 1',
         'PUSH 1',
         'LOOP:',
@@ -14,18 +14,75 @@ def main():
         'MOV AX',
         'MUL',
         'PUSH AX',
-        'PUSH 1',
-        'ADD',
+        'INC',
         'JMP LOOP',
         'END_LOOP:',
         'POP',
+        'STR RESULT: ',
+        'PRINT ,',
+        'PRINT',
         'HALT'
     ]
 
+    program_sqrt_144 = [
+        'PUSH 144',
+        'SQRT',
+        'STR RESULT: ',
+        'PRINT ,',
+        'PRINT',
+        'HALT'
+    ]
+
+    program_900 = [
+        "PUSH 10",
+        "PUSH 20",
+        "ADD",
+        "PUSH 30",
+        "MUL",
+        'STR RESULT: ',
+        'PRINT ,',
+        "PRINT",
+        "HALT"
+    ]
+
+    program_sum_1000 = [
+        "start:",
+        "PUSH 0",  # Push the initial sum (0) onto the stack
+        "PUSH 1",  # Push the start number (1) onto the stack
+        "LOOP:",
+        "DUP",
+        "PUSH 1000",
+        "CMP",  # Compare the current number with the end number
+        "JG end_loop",  # If the current number is greater than the end number, jump to the loop label
+        "MOV AX",
+        "ADD",
+        "PUSH AX",
+        "INC",
+        "JMP LOOP",
+        "end_loop:",
+        'POP',
+        'STR RESULT: ',
+        'PRINT ,',
+        'PRINT',
+        'STR Cycle: ',
+        'PRINT ,',
+        'CYCLE',
+        'PRINT',
+        "HALT",  # Stop the program
+    ]
+
     stack_machine = StackMachine()
-    stack_machine.load_program(program)
+    stack_machine.load_program(program_10_factorial)
     stack_machine.run()
-    print("Result:", stack_machine.stack[-1])
+
+    stack_machine.load_program(program_sqrt_144)
+    stack_machine.run()
+
+    stack_machine.load_program(program_900)
+    stack_machine.run()
+
+    stack_machine.load_program(program_sum_1000)
+    stack_machine.run()
 
 
 if __name__ == '__main__':
--- simple_vm/stack_machine.py
+++ simple_vm/stack_machine.py
@@ -1,49 +1,68 @@
+import math
 from enum import Enum
 
 
 class Command(Enum):
-    PUSH = 'PUSH'
-    POP = 'POP'
-    ADD = 'ADD'
-    SUB = 'SUB'
-    MUL = 'MUL'
-    DIV = 'DIV'
-    MOD = 'MOD'
-    DUP = 'DUP'
-    SWAP = 'SWAP'
-    JMP = 'JMP'
-    JG = 'JG'
-    JL = 'JL'
-    JZ = 'JZ'
-    JNZ = 'JNZ'
-    CMP = 'CMP'
-    MOV = 'MOV'
-    NOP = 'NOP'
-    HALT = 'HALT'
-    LABEL = 'LABEL'
+    ADD = 'ADD'               # Add the top two values on the stack
+    CMP = 'CMP'               # Compare the top two values on the stack
+    DEC = 'DEC'               # Decrement the top value on the stack
+    CYCLE = 'CYCLE'           # Print cycle
+    DIV = 'DIV'               # Divide the top two values on the stack
+    DUP = 'DUP'               # Duplicate the top value on the stack
+    HALT = 'HALT'             # Halt program execution
+    INC = 'INC'               # Increment the top value on the stack
+    JMP = 'JMP'               # Unconditionally jump to a label
+    JG = 'JG'                 # Jump to a label if the top value on the stack is greater than 0
+    JL = 'JL'                 # Jump to a label if the top value on the stack is less than 0
+    JNZ = 'JNZ'               # Jump to a label if the top value on the stack is not zero
+    JZ = 'JZ'                 # Jump to a label if the top value on the stack is zero
+    LABEL = 'LABEL'           # Define a label in the program
+    MOD = 'MOD'               # Calculate the remainder of the top two values on the stack
+    MOV = 'MOV'               # Move the top value on the stack to a register
+    MUL = 'MUL'               # Multiply the top two values on the stack
+    NOP = 'NOP'               # Do nothing
+    POP = 'POP'               # Remove the top value from the stack
+    PRINT = 'PRN'             # Print the top value from the stack
+    PUSH = 'PUSH'             # Push a value or register onto the stack
+    SQRT = 'SQRT'             # Calculate the square root of the top value on the stack
+    STR = 'STR'               # Push a string onto the stack
+    SUB = 'SUB'               # Subtract the top two values on the stack
+    SWAP = 'SWAP'             # Swap the top two values on the stack
 
 
 class StackMachine:
     """
-    PUSH <value>: Push a value onto the stack. e.g., PUSH 10
-    POP: Remove and return the top value from the stack.
+    The StackMachine class implements a simple stack-based virtual machine.
+
+    Available commands:
     ADD: Remove the top two values from the stack, calculate their sum, and push the result onto the stack.
-    SUB: Remove the top two values from the stack, calculate their difference, and push the result onto the stack.
-    MUL: Remove the top two values from the stack, calculate their product, and push the result onto the stack.
-    DIV: Remove the top two values from the stack, calculate their division, and push the result onto the stack.
-    MOD: Remove the top two values from the stack, calculate their remainder, and push the result onto the stack.
-    DUP: Copy the top value from the stack and push it onto the stack.
-    SWAP: Swap the top two values on the stack.
-    JMP <label>: Move program execution to the given label. e.g., JMP LOOP
-    JG <label>: If the top value on the stack is greater than 0, move program execution to the given label.
-    JL <label>: If the top value on the stack is less than 0, move program execution to the given label.
-    JZ <label>: If the top value on the stack is 0, move program execution to the given label.
-    JNZ <label>: If the top value on the stack is not 0, move program execution to the given label.
-    CMP: Compare the top two values on the stack and push the result onto the stack.
-    (-1: First value is smaller, 0: Both values are equal, 1: First value is larger)
-    MOV <register>: Move the top value from the stack to the specified register (AX, BX, CX, or DX).
-    NOP: Perform no operation. This command is ignored in the program.
+    CMP: Compare the top two values on the stack and push the result onto the stack (-1, 0, or 1).
+    DEC: Decrement the top value on the stack by 1 and push the result onto the stack.
+    DIV: Remove the top two values from the stack, calculate their integer division, and push the result onto the stack.
+    DUP: Duplicate the top value on the stack.
     HALT: Stop program execution.
+    INC: Increment the top value on the stack by 1 and push the result onto the stack.
+    JMP <label>: Unconditionally jump to the specified label in the program.
+    JG <label>: Jump to the specified label if the top value on the stack is greater than 0.
+    JL <label>: Jump to the specified label if the top value on the stack is less than 0.
+    JNZ <label>: Jump to the specified label if the top value on the stack is not 0.
+    JZ <label>: Jump to the specified label if the top value on the stack is 0.
+    LABEL: Define a label in the program for use with jump commands.
+    MOD: Remove the top two values from the stack, calculate their remainder, and push the result onto the stack.
+    MOV <register>: Move the top value from the stack to the specified register (AX, BX, CX, or DX).
+    MUL: Remove the top two values from the stack, calculate their product, and push the result onto the stack.
+    NOP: Perform no operation. This command is ignored in the program.
+    POP: Remove and discard the top value from the stack.
+    PRINT <separator>: Print the top value of the stack, optionally followed by a separator character.
+    PUSH <value>: Push a value or the content of a register onto the stack. e.g., PUSH 10 or PUSH AX
+    SQRT: Remove the top value from the stack, calculate its square root, and push the result onto the stack.
+    STR <value>: Push a string onto the stack.
+    SUB: Remove the top two values from the stack, calculate their difference, and push the result onto the stack.
+    SWAP: Swap the top two values on the stack.
+
+    The StackMachine class provides methods for loading a program, parsing commands,
+    executing commands, and running the program. It also maintains a stack, a set of registers,
+    and a list of labels defined in the program.
     """
 
     def __init__(self):
@@ -53,35 +72,48 @@ class StackMachine:
         self.registers = {'AX': 0, 'BX': 0, 'CX': 0, 'DX': 0}
         self.command_list = {}
         self.instruction_pointer = 0
+        self.cycle = 0
         self._init_command_list()
 
     def _init_command_list(self):
-        self.command_list[Command.PUSH] = self.command_push
-        self.command_list[Command.POP] = self.command_pop
-        self.command_list[Command.ADD] = self.command_add
-        self.command_list[Command.SUB] = self.command_sub
-        self.command_list[Command.MUL] = self.command_mul
-        self.command_list[Command.DIV] = self.command_div
-        self.command_list[Command.MOD] = self.command_mod
-        self.command_list[Command.DUP] = self.command_dup
-        self.command_list[Command.SWAP] = self.command_swap
-        self.command_list[Command.JMP] = self.command_jmp
-        self.command_list[Command.JG] = self.command_jg
-        self.command_list[Command.JL] = self.command_jl
-        self.command_list[Command.JZ] = self.command_jz
-        self.command_list[Command.JNZ] = self.command_jnz
-        self.command_list[Command.CMP] = self.command_cmp
-        self.command_list[Command.MOV] = self.command_mov
-        self.command_list[Command.NOP] = self.command_nop
-        self.command_list[Command.HALT] = self.command_halt
-        self.command_list[Command.LABEL] = self.command_label
+        self.command_list = {
+            Command.ADD: self.command_add,
+            Command.CMP: self.command_cmp,
+            Command.CYCLE: self.command_cycle,
+            Command.DEC: self.command_dec,
+            Command.DIV: self.command_div,
+            Command.DUP: self.command_dup,
+            Command.HALT: self.command_halt,
+            Command.INC: self.command_inc,
+            Command.JMP: self.command_jmp,
+            Command.JG: self.command_jg,
+            Command.JL: self.command_jl,
+            Command.JNZ: self.command_jnz,
+            Command.JZ: self.command_jz,
+            Command.LABEL: self.command_label,
+            Command.MOD: self.command_mod,
+            Command.MOV: self.command_mov,
+            Command.MUL: self.command_mul,
+            Command.NOP: self.command_nop,
+            Command.POP: self.command_pop,
+            Command.PRINT: self.command_print,
+            Command.PUSH: self.command_push,
+            Command.SQRT: self.command_sqrt,
+            Command.STR: self.command_string,
+            Command.SUB: self.command_sub,
+            Command.SWAP: self.command_swap
+        }
 
     def load_program(self, program):
+        self.stack.clear()
         self.program = program
         self.labels = {line[:-1]: i for i, line in enumerate(program) if line.endswith(':')}
+        self.instruction_pointer = 0
+        self.cycle = 0
 
     def parse_command(self, command_string):
         command, *args = command_string.split()
+
         if command not in Command.__members__:
             if ':' in command:
                 return Command['LABEL'], args
@@ -122,8 +154,20 @@ class StackMachine:
         b, a = self.stack.pop(), self.stack.pop()
         self.stack.append(a % b)
 
+    def command_inc(self, args):
+        self.stack.append(self.stack.pop() + 1)
+
+    def command_dec(self, args):
+        self.stack.append(self.stack.pop() - 1)
+
+    def command_sqrt(self, args):
+        self.stack.append(math.sqrt(self.stack.pop()))
+
     def command_dup(self, args):
         self.stack.append(self.stack[-1])
+
+    def command_cycle(self, args):
+        self.stack.append(self.cycle)
 
     def command_swap(self, args):
         self.stack[-1], self.stack[-2] = self.stack[-2], self.stack[-1]
@@ -163,10 +207,22 @@ class StackMachine:
     def command_label(self, args):
         pass
 
+    def command_print(self, args):
+        if ';' in args:
+            print(self.stack.pop(), end='')
+        elif ',' in args:
+            print(self.stack.pop(), end=' ')
+        else:
+            print(self.stack.pop())
+
+    def command_string(self, args):
+        self.stack.append(' '.join(args))
+
     def run(self):
         for cycle in range(20000):
+            self.cycle = cycle
             if self.instruction_pointer >= len(self.program):
-                print(f"Cycle: {cycle}")
+                #print(f"Cycle: {cycle}")
                 break
             command_string = self.program[self.instruction_pointer]
             command, args = self.parse_command(command_string)
